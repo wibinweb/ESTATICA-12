a12ce4ddb26ff7dcb782df2fad3de8850b6b7638
Pablo <pablonietoperez369@gmail.com>
2024-05-09 00:43:44 -0500

    first commit

---
+++ JS/script.js
@@ -0,0 +1,47 @@
+$(function(){
+  $(".Caja-1").click (function(e){
+    e.preventDefault();
+    $(".Caja-1 p:nth-child(3)").toggleClass("open");
+  });
+});
+
+$(function(){
+  $(".Caja-2").click (function(e){
+    e.preventDefault();
+    $(".Caja-2 p:nth-child(3)").toggleClass("open");
+  });
+});
+
+$(function(){
+  $(".Caja-3").click (function(e){
+    e.preventDefault();
+    $(".Caja-3 p:nth-child(3)").toggleClass("open");
+  });
+});
+
+$(function(){
+  $(".Caja-4").click (function(e){
+    e.preventDefault();
+    $(".Caja-4 p:nth-child(3)").toggleClass("open");
+  });
+});
+
+$(function(){
+  $(".Caja-5").click (function(e){
+    e.preventDefault();
+    $(".Caja-5 p:nth-child(3)").toggleClass("open");
+  });
+});
+
+$(function(){
+  $(".Caja-6").click (function(e){
+    e.preventDefault();
+    $(".Caja-6 p:nth-child(3)").toggleClass("open");
+  });
+});
+
+
+
+
+
+
